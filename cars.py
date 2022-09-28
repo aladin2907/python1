1c21bd5510c4d5eaecc3f1f11baef303e6d9ec05
Vadym Nahornyi <uvn2203@gmail.com>
2022-09-28 16:38:48 +0300

    Commit finile

--- cars.py
+++ cars.py
@@ -7,7 +7,7 @@ class Toyota:
         id_new = Abstractnyi.generate_new_id()
         self.car = {id_new: {"Color": input('What color ?'), "engine: ": input('Engine ?')}}
 
-    def add_to_list(self):
+    def add_car_to_list(self):
         print('Adding new car to file')
         myfile1 = open("DBpy/dbp.json", 'a')
         myfile1.write(json.dumps(self.car) + '\n')
@@ -27,7 +27,7 @@ class Abstractnyi:
     def change_color():
         print('Chenging color')
         id_car = int(input('What ID do you want change ?'))
-        if Abstractnyi.generate_new_id()> id_car > 0:
+        if Abstractnyi.generate_new_id() > id_car > 0:
             data = Abstractnyi.read_data(id_car)
             new_color = input('What is new color ?')
             Abstractnyi.write_data(data, id_car, new_color)
@@ -36,27 +36,21 @@ class Abstractnyi:
             print('Car with this id not found')
 
     def read_data(id):
-        print('read_data', id)
-        i = 1
+        print('read_data')
         with open("DBpy/dbp.json", 'r') as myfile:
-            for line in myfile:
-                data1 = line
-                print('liniya nomer', i, line)
-                if i == id:
-                    data = json.loads(line)
-                    myfile.close()
-                    print('Prochitali', data)
-                    return data
-                i += 1
-
-    def write_data(data, id, new_color):
-        data[str(id)]['Color'] = new_color
-        print('new color', data)
-        with open("DBpy/dbp.json", 'r+') as myfile:
+            data = []
             i = 1
             for line in myfile:
-                if id == i:
-                    print('menyaem')
-                    myfile.write(json.dump(data))
+                data.append(json.loads(line))
                 i += 1
+        print('Prochitali', len(data))
+        return data
 
+    def write_data(data, id, new_color):
+        data[id - 1][str(id)]['Color'] = new_color
+        with open("DBpy/dbp.json", 'r+') as myfile:
+            j = 0
+            for i in data:
+                myfile.write(json.dumps(data[j]) + '\n')
+                # myfile.write(json.dumps(data[i]))
+                j += 1
--- main.py
+++ main.py
@@ -5,11 +5,9 @@ while True:
     flag = int(input('What do you want ? 1 - create new car 2 - change car '))
     if flag == 1:
         newcar = Toyota()
-        newcar.add_to_list()
+        newcar.add_car_to_list()
 
     elif flag == 2:
         Abstractnyi.change_color()
     else:
-        print('Attention !')
-
-    # test commit
+        print('Attention, check your answer!')
